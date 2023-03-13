e53057ca273dc0823928643717cc19beacafa710
zThanapong <gg.alphaone@gmail.com>
2023-03-13 14:29:05 +0700

    init Project

---
+++ apps/admin/src/app/App.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Header } from '@nx101/ui';
+
+export function App() {
+  return (
+    <div>
+      <Header></Header>
+      Hello Admin
+    </div>
+  );
+}
+
+export default App;
